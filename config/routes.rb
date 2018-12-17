fad16d3d726978f6ef6c7a2b007654998a243d88
ishida_masakazu <u225210b@gmail.com>
2018-12-18 00:13:55 +0900

    ログイン機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   root to: 'tasks#index'
 
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
   namespace :admin do
     resources :users
   end
-  
+
   resources :tasks
 end
